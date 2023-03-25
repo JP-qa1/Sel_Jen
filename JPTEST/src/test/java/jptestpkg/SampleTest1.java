a6732d0ce71935135802e5c487b559f9610c49d2
Jhansi <jsp_us@yahoo.com>
2023-03-24 23:54:27 -0400

    1st commit

---
+++ JPTEST/src/test/java/jptestpkg/SampleTest1.java
@@ -0,0 +1,14 @@
+package jptestpkg;
+
+import org.testng.annotations.Test;
+
+public class SampleTest1 
+
+{
+	@Test(groups= {"SmokeTest"})
+	public void method1()
+	{
+		System.out.println("Method1 is running");
+	}
+
+}
---
+++ JPTEST/src/test/java/jptestpkg/SampleTest3.java
@@ -0,0 +1,12 @@
+package jptestpkg;
+
+import org.testng.annotations.Test;
+
+public class SampleTest3 
+{
+	@Test(groups= {"SmokeTest","RegressionTest"})
+	public void method3()
+	{
+		System.out.println("Method3 is running");
+	}
+}
